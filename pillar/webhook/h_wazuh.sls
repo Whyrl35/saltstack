37feba546e386440a3f45a1c49ee6af4c9dc64ef
Ludovic Houdayer <ludovic.houdayer@gmail.com>
2021-01-18 09:55:53 +0100

    wigo to slack

--- pillar/webhook/h_wazuh.sls
+++ pillar/webhook/h_wazuh.sls
@@ -3,14 +3,14 @@
 {% set secret = salt['vault'].read_secret('secret/salt/web/webhook') %}
 
 webhooks:
-  files:
-    - /opt/webhooks/actions/wigo-to-slack.py:
-      - source: salt://webhook/files/wigo-to-slack.py
+  #files:
+  #  - /opt/webhooks/actions/wigo-to-slack.py:
+  #    - source: salt://webhook/files/wigo-to-slack.py
   configurations:
     - id: "wigo-to-slack"
       execute-command: "/opt/webhooks/actions/wigo-to-slack.py"
       pass-arguments-to-command:
-        - source: body
+        - source: payload
           name: Notification
       command-working-directory: "/tmp"
       reponses-message: "sending message to slack..."
--- salt/webhook/files/wigo-to-slack.py
+++ salt/webhook/files/wigo-to-slack.py
@@ -1,18 +1,51 @@
 #!/usr/bin/env python3
 
 import sys
+import json
+import requests
 
-TEST_FILE = "/tmp/test-wigo-to-slack.txt"
+TEST_FILE = "/tmp/test-wigo-to-slack.json"
+URL_SLACK = ""
 
 
 def main(argv):
     if len(argv) < 2:
         print("Missing arguments!")
         sys.exit(1)
-    with open(TEST_FILE, 'w') as tf:
-        tf.write(argv)
-        tf.close()
+
+    try:
+        notification = json.loads(sys.argv[1])
+    except AttributeError:
+        print("Wrong json format to load as argv[1]")
+        sys.exit(2)
+
+    slack_data = dict()
+    slack_data['text'] = notification['Message']
+    slack_data['blocks'] = [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": ":fire: WIGO noticed an issue",
+                "emoji": True
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "{}\nOn: {}".format(notification['Message'], notification['Date']),
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Summary: {}".format(notification['Summary']),
+            }
+        }
+    ]
+    requests.post(URL_SLACK, json=slack_data)
 
 
-if "__name__" == "__main__":
-    main(sys.argv)
+main(sys.argv)
